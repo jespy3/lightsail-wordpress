84c915946201edc1f4831ae934c58b87c21bc98f
josh-espinas <josh.espinas@xero.com>
2024-10-06 15:20:24 +0100

    Adds a user data script that says hello world

--- infra/core/lightsail.tf
+++ infra/core/lightsail.tf
@@ -5,6 +5,7 @@ resource "aws_lightsail_instance" "wordpress_and_db" {
   bundle_id         = "nano_3_0"
   key_pair_name     = local.ls_keypair
   ip_address_type   = "ipv4"
+  user_data         = file("${path.module}/user-data.sh")
 }
 
 resource "aws_lightsail_key_pair" "ls_kp" {
