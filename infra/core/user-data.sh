2518a0e1a941b414cd327cf3f2e289dcccc8b01b
josh-espinas <josh.espinas@xero.com>
2024-10-06 16:43:34 +0100

    Update user data to download GPG keys

--- infra/core/user-data.sh
+++ infra/core/user-data.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-echo "Hello World!"
+echo "Hello World! Executing the user-data script to install Docker and Docker Compose."
+
+# Removing potential conflicting packages with the Docker installation
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do \
+  apt remove $pkg; \
+done
+
+# Add Docker's official GPG key
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
