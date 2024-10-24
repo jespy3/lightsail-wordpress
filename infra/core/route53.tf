2eeda1e0ad01202869f1bc59b6584e7f7647c929
josh-espinas <josh.espinas@xero.com>
2024-10-24 18:16:59 +0100

    Points URLs to port 80 instead of 8080 too

--- infra/core/route53.tf
+++ infra/core/route53.tf
@@ -10,3 +10,11 @@ resource "aws_route53_record" "www" {
   records = [aws_lightsail_instance.wordpress_and_db.public_ip_address]
 }
 
+resource "aws_route53_record" "no_www" {
+  zone_id = data.aws_route53_zone.primary.zone_id
+  name    = "${local.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_lightsail_instance.wordpress_and_db.public_ip_address]
+}
+
